adf5dab46327e222de7961444fe8b6077e72485c
Muffasa <ohadgalor1990@gmail.com>
2015-12-07 13:44:35 +0200

    v1.0

--- www/js/init/init.js
+++ www/js/init/init.js
@@ -6,8 +6,8 @@ angular.module("PresencePOC")
    $ionicPlatform.ready(function(){
     //navigator.splashscreen.show()
     //$rootScope.serverUrl= 'https://node-dev-env1-muffasa.c9users.io:8080/'
-    $rootScope.serverUrl= 'https://presence-node-clean-muffasa.c9users.io:8080/'
-    //$rootScope.serverUrl = 'http://188.166.98.191:8888/'
+    //$rootScope.serverUrl= 'https://presence-node-clean-muffasa.c9users.io:8080/'
+    $rootScope.serverUrl = 'http://188.166.98.191:8888/'
     $rootScope.settingsRadius = 50
     $rootScope.settingsUpTime = 15 
      //return $state.go('work-space')
